312d2115808631c5b634b7ea7c4542d3597665de
LWingKi <an10335@gmail.com>
2022-06-08 17:58:53 +0200

    robif2b with wheel alignment code US2

---
+++ robif2b/build/src/nbx/ethercat/CMakeFiles/ethercat.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wing/Documents/SDP/sdp_ws/robif2b/src/nbx/ethercat/ethercat.c" "/home/wing/Documents/SDP/sdp_ws/robif2b/build/src/nbx/ethercat/CMakeFiles/ethercat.dir/ethercat.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ethercat_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/wing/Documents/SDP/sdp_ws/install/include/soem"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
