5313814b965fc4ce7d553a7f93ec5f1b0c5cbaed
Barath19 <bk190196@gmail.com>
2022-06-12 12:45:57 +0200

    ROB, if to be?? Userstory 3

---
+++ deliverables_3/robif2b/build/src/nbx/kelo/CMakeFiles/kelo.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/barath/sdp/robif2b/src/nbx/kelo/kelo_drive.c" "/home/barath/sdp/robif2b/build/src/nbx/kelo/CMakeFiles/kelo.dir/kelo_drive.c.o"
+  "/home/barath/sdp/robif2b/src/nbx/kelo/kelo_power_board.c" "/home/barath/sdp/robif2b/build/src/nbx/kelo/CMakeFiles/kelo.dir/kelo_power_board.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "kelo_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
