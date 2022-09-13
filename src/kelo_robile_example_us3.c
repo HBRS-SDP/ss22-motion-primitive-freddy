deb53f272c41f6709f1564d835b97c0d43fe42de
kvnptl <kevinpatel4400@gmail.com>
2022-09-13 14:29:01 +0200

    update: remove wheel allignment values

--- src/kelo_robile_example_us3.c
+++ src/kelo_robile_example_us3.c
@@ -181,110 +181,34 @@ int main(int argc, char *argv[])
     if (state.ecat.error_code < 0)
         return -1;
 
-    // init
-    double pi = 3.14;
-    double max_angle = 6.28;
-    // char *ptr;
-    double angle = 90;
-    // angle = strtod(argv[1], &ptr);
-    //  printf(fmod((2.10+pi),max_angle));
-    double setpoint[4] = {0, 0, 0, 0};
-
-    if (angle == 0)
-    {
-
-        // POS X
-        setpoint[0] = 2.10;
-        setpoint[1] = 3.47;
-        setpoint[2] = 2.94;
-        setpoint[3] = 0.6;
-    }
-
-    // double thrd_pos_x[4] = {2.10,3.47,2.94,0.6};
-    //  TODO: use fmod using math.h library
-    //  double thrd_neg_x[4] = {fmod(2.10+pi,max_angle),fmod(0.33+pi,max_angle),fmod(2.94+pi,max_angle),fmod(0.6+pi,max_angle)};
-
-    else if (angle == 90)
-    {
-
-        // POS Y
-        setpoint[0] = fmod(2.10 + pi / 2, max_angle);
-        setpoint[1] = fmod(3.47 + pi / 2, max_angle);
-        setpoint[2] = fmod(2.94 + pi / 2, max_angle);
-        setpoint[3] = fmod(0.60 + pi / 2, max_angle);
-
-        // setpoint[0] = 2.10 + pi/2;
-        // setpoint[1] = 3.47 + pi/2;
-        // setpoint[2] = 2.94 + pi/2;
-        // setpoint[3] = 0.60 + pi/2;
-
-        // setpoint[4] = {2.10+pi/2, 3.47+pi/2, 2.94+pi/2, 0.6+pi/2};
-    }
-    else if (angle == 180)
-    {
-
-        // NEG X
-
-        setpoint[0] = fmod(2.10 + pi, max_angle);
-        setpoint[1] = fmod(3.47 + pi, max_angle);
-        setpoint[2] = fmod(2.94 + pi, max_angle);
-        setpoint[3] = fmod(0.60 + pi, max_angle);
-
-        // setpoint[0] = 2.10 + pi;
-        // setpoint[1] = 0.33;
-        // setpoint[2] = 2.94 + pi;
-        // setpoint[3] = 0.60 + pi;
-
-        // double setpoint[4] = {2.10+pi, 0.33, 2.94+pi, 0.6+pi};
-    }
-    else if (angle == 270)
-    {
-        // NEG Y
-
-        setpoint[0] = fmod(2.10 + 1.5 * pi, max_angle);
-        setpoint[1] = fmod(3.47 + 1.5 * pi, max_angle);
-        setpoint[2] = fmod(2.94 + 1.5 * pi, max_angle);
-        setpoint[3] = fmod(0.60 + 1.5 * pi, max_angle);
-
-        // setpoint[0] = 0.53;
-        // setpoint[1] = 1.9;
-        // setpoint[2] = 1.37;
-        // setpoint[3] = 0.60 + 1.5*pi;
-
-        // double setpoint = {0.53, 1.9, 1.37, 0.6+1.5*pi};
-    }
-
-    int stop_wheel_counter[4] = {0, 0, 0, 0};
-    bool isAligned = true;
-
-    bool inMotion = false;
-
-    // double thrd_neg_x[4] = {2.10+pi, 0.33, 2.94+pi, 0.6+pi};
-    // double thrd_pos_y[4] = {2.10+pi/2, 3.47+pi/2, 2.94+pi/2, 0.6+pi/2};
-    //  double thrd_neg_y[4] = {2.10+1.5*pi, 3.47+1.5*pi, 2.94+1.5*pi, 0.6+1.5*pi};
-    // double thrd_neg_y[4] = {0.53, 1.9, 1.37, 0.6+1.5*pi};
-
-    FILE *fpt;
-    fpt = fopen("ramp_log_robif2b_04.csv", "w+");
-    fprintf(fpt, "drive id,wheel_velocity1,wheel_velocity2,pivot_position,pivot_velocity,imu_ang_vel_x,imu_ang_vel_y,imu_ang_vel_z,imu_lin_acc_x,imu_lin_acc_y,imu_lin_acc_z,voltage,current\n");
+    double motor_trq = 0.5;
 
     while (true)
     {
         if (state.ecat.error_code < 0)
             return -1;
-        if (isAligned)
+
+        state.kelo_cmd.trq[0] = -motor_trq; //-1.4
+        state.kelo_cmd.trq[1] = motor_trq;  // 1.4
+        state.kelo_cmd.trq[2] = motor_trq;  //-1.4
+        state.kelo_cmd.trq[3] = motor_trq;  // 1.4
+        state.kelo_cmd.trq[4] = -motor_trq; //-1.4
+        state.kelo_cmd.trq[5] = motor_trq;  // 1.4
+        state.kelo_cmd.trq[6] = -motor_trq; //-1.4
+        state.kelo_cmd.trq[7] = motor_trq;  // 1.4
+
+        for (int i = 0; i < NUM_DRIVES; i++)
         {
 
-            printf("Linear Acceleration : %f \n", state.kelo_msr.imu_lin_acc[0]);
-            printf("Angular Velocity : %f \n", state.kelo_msr.imu_ang_vel[0]);
-            state.kelo_cmd.trq[0] = -1.4; //-1.4
-            state.kelo_cmd.trq[1] = 1.4;  // 1.4
-            // state.kelo_cmd.trq[2] = -1.4;      //-1.4
-            // state.kelo_cmd.trq[3] =  1.4;      // 1.4
-            // state.kelo_cmd.trq[4] = -1.4;      //-1.4
-            // state.kelo_cmd.trq[5] =  1.4;      // 1.4
-            // state.kelo_cmd.trq[6] = -1.4;      //-1.4
-            // state.kelo_cmd.trq[7] =  1.4;      // 1.4
+            printf("drive [id=%i, conn=%i]: "
+                   "w_vel[0]=%5.2f - w_vel[1]=%5.2f - p_pos=%5.2f - w_pos[0]=%5.2f - w_pos[1]=%5.2f\n\n",
+                   i, state.ecat.is_connected[i + 1],
+                   state.kelo_msr.whl_vel[i * 2 + 0],
+                   state.kelo_msr.whl_vel[i * 2 + 1],
+                   state.kelo_msr.pvt_pos[i],
+                   state.kelo_msr.whl_pos[i * 2 + 0],
+                   state.kelo_msr.whl_pos[i * 2 + 1]);
+            // double pvangle[NUM_DRIVES] = state.kelo_msr.pvt_pos[i]
         }
 
         clock_gettime(CLOCK_MONOTONIC, &state.time.cycle_start);
@@ -294,47 +218,6 @@ int main(int argc, char *argv[])
         robif2b_kelo_drive_actuator_update(&wheel_act);
         robif2b_kelo_power_board_update(&power_board);
 
-        for (int i = 0; i < NUM_DRIVES; i++)
-        {
-            // printf("drive [id=%i, conn=%i]: "
-            //         "w_vel[0]=%5.2f - w_vel[1]=%5.2f - p_pos=%5.2f ,pvt_vel=%5.2f, imu_ang_vel= %5.2f ,imu_lin_acc=%5.2f\n",
-            //         i, state.ecat.is_connected[i + 1],
-            //         state.kelo_msr.whl_vel[i * 2 + 0],
-            //         state.kelo_msr.whl_vel[i * 2 + 1],
-            //         state.kelo_msr.pvt_pos[i],
-            //         state.kelo_msr.pvt_vel[i],
-            //         state.kelo_msr.imu_ang_vel[i],
-            //         state.kelo_msr.imu_ang_vel[i+1],
-            //         state.kelo_msr.imu_ang_vel[i+2],
-            //         state.kelo_msr.imu_lin_acc[i],
-            //         state.kelo_msr.imu_lin_acc[i+1],
-            //         state.kelo_msr.imu_lin_acc[i+2]);
-            fprintf(fpt, "%i,%5.2f,%5.2f,%5.2f,%5.2f,%5.2f,%5.2f,%5.2f,%5.2f,%5.2f,%5.2f,,%5.2f,%5.2f\n",
-                    i,
-                    state.kelo_msr.whl_vel[i * 2 + 0],
-                    state.kelo_msr.whl_vel[i * 2 + 1],
-                    state.kelo_msr.pvt_pos[i],
-                    state.kelo_msr.pvt_vel[i],
-                    state.kelo_msr.imu_ang_vel[i],
-                    state.kelo_msr.imu_ang_vel[i + 1],
-                    state.kelo_msr.imu_ang_vel[i + 2],
-                    state.kelo_msr.imu_lin_acc[i],
-                    state.kelo_msr.imu_lin_acc[i + 1],
-                    state.kelo_msr.imu_lin_acc[i + 2],
-                    state.kelo_msr.bat_volt,
-                    state.kelo_msr.bat_cur);
-            // double pvangle[NUM_DRIVES] = state.kelo_msr.pvt_pos[i]
-        }
-
-        // printf("power board conn=%i: "
-        //        "b_volt=%5.2f - b_cur[1]=%5.2f - b_pwr=%5.2f - b_lvl=%5.2i\n",
-        //         state.ecat.is_connected[0],
-        //         state.kelo_msr.bat_volt,
-        //         state.kelo_msr.bat_cur,
-        //         state.kelo_msr.bat_pwr,
-        //         state.kelo_msr.bat_lvl);
-        // printf("\n");
-
         clock_gettime(CLOCK_MONOTONIC, &state.time.cycle_end);
         state.time.cycle_time_msr = timespec_to_usec(&state.time.cycle_end) - timespec_to_usec(&state.time.cycle_start);
 
@@ -343,8 +226,6 @@ int main(int argc, char *argv[])
             usleep(state.time.cycle_time_exp - state.time.cycle_time_msr);
         }
     }
-
-    fclose(fpt);
 
     robif2b_kelo_drive_actuator_stop(&wheel_act);
     robif2b_ethercat_stop(&ecat);
